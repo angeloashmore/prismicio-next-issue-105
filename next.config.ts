1e94ff0cee01713f94873a061eb0083515625a35
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2025-02-26 11:47:39 -1000

    chore: simplify further

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,2 @@
-import type { NextConfig } from "next";
-
 import {enableAutoPreviews} from '@prismicio/next'
 enableAutoPreviews
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
